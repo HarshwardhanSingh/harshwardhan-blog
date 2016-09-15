1449a76e9ac1d397c33f8a7dd0ef9de5368f898d
Harshwardhan Singh Rathore <harsh.rathore14@gmail.com>
2016-09-15 11:37:58 +0530

    adds signin functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  def current_user
+    @current_user ||= Admin.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def authenticate_user!
+    redirect_to new_session_path, flash: { error: "You need to sign in for that!!" } if session[:user_id].blank?
+  end
 end
